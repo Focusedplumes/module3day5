dbe301b0e208b4d5155076848d5d4f17cd840924
Focusedplumes <152942773+Focusedplumes@users.noreply.github.com>
2024-04-24 18:34:40 -0500

    end of class stuff

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+import fs from 'fs';
 import _ from 'lodash';
-import { color, log, red, green } from 'console-log-colors';
+import { color, log, red, green, blue } from 'console-log-colors';
+import request from 'request';
 
 const myarray = [1, 2, 3, 4, 5, 8, 4, 3, 9];
 
@@ -9,3 +11,20 @@ const output = _.uniq(myarray)
 
 console.log(output);
 
+//Sync File Access the program stops running until every else below completes <-Bad
+const file = fs.readFileSync('./data.json', {encoding: 'utf8'});
+console.log(color.red(file));
+
+//An ASync File Access  mode <-Good
+fs.readFile('./data.json', {encoding: 'utf8'}, (err, f) => console.log(color.gree(f)));
+
+request('https://jsonplaceholder.typicode.com/posts', (err, resp, body) => {
+ const parsed = JSON.parse(body);
+ parsed.forEach( (x) => console.log(color.blue(x)) );
+ 
+});
+
+//An example of printing JSON data as a string is: JSON.stringify(parsed);
+// console.log(JSON.stringify(parsed))
+
+// for example
