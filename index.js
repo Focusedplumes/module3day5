4cc132ee80da5602f5baca10eb6b43bb359ecb86
Focusedplumes <152942773+Focusedplumes@users.noreply.github.com>
2024-04-26 07:58:24 -0500

    updating

--- index.js
+++ index.js
@@ -11,18 +11,21 @@ const output = _.uniq(myarray)
 
 console.log(output);
 
+// echo 'node_modules/*' > .gitignore
+// git add -A
+// git commit -m "Some message"
+
 //Sync File Access the program stops running until every else below completes <-Bad
 const file = fs.readFileSync('./data.json', {encoding: 'utf8'});
 console.log(color.red(file));
 
 //An ASync File Access  mode <-Good
-fs.readFile('./data.json', {encoding: 'utf8'}, (err, f) => console.log(color.green(f)));
+fs.readFile('./data.json', {encoding: 'utf8'}, (err,f) => console.log(color.green(f)));
 
 request('https://jsonplaceholder.typicode.com/posts', (err, resp, body) => {
- const parsed = JSON.parse(body);
- parsed.forEach( (x) => console.log(color.blue(x)) );
- fs.writeFileSync("./newWebStuff.json", JSON.stringify(body));
- 
+    const parsed = JSON.parse(body);
+    parsed.forEach( (x) => console.log(x));
+    fs.writeFileSync('./newWebStuff.json', JSON.stringify(parsed));
 });
 
 //An example of printing JSON data as a string is: JSON.stringify(parsed);
